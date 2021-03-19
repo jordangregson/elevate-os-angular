d779b951be8943dbf5fbcaa8d27497e217e0c0dc
Jordan Gregson <49405277+jordangregson@users.noreply.github.com>
2021-03-19 11:02:09 -0400

    Made final adjustments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    SignInComponent
   ],
   imports: [
     BrowserModule,
